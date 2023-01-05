9254b045489a64085042f2ad6f40a324aa38bb29
Lizz3108 <lizbethgarcialebron@gmail.com>
2023-01-05 03:35:12 +0000

    Working with edge cases

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,8 +11,13 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node->left == NULL || node->right == NULL)
-		return (1);
+	if (node->left != NULL)
+	{
+		if (node->right != NULL)
+			return (1);
+		else
+			return (0);
+	}
 	else
 		return (0);
 
