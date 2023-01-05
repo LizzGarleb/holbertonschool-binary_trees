1488faf1e21bd47d700e8dc285f54a80c8d5197f
Lizz3108 <lizbethgarcialebron@gmail.com>
2023-01-05 04:17:34 +0000

    task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -13,9 +13,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 		return (0);
 
 	if (node->left != NULL || node->right != NULL)
-		return (1);
+		return (0);
 	else if (node->left == NULL && node->right == NULL)
-		return (1);
-	else
 		return (0);
 }
