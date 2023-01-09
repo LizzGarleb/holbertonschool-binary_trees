661788cb293309da4a719de38c2f04c64ed1e2c6
Lizz3108 <lizbethgarcialebron@gmail.com>
2023-01-09 13:21:52 +0000

    Task 7 betty

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,6 +1,8 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_inordedr - that goes throught a binary tree using in-order
+ * @tree: pointer to root
+ * @func: pointer to function to call for each node
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
