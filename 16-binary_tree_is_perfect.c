c30d306c43a35dff1555328ffc104437fa233018
Lizz3108 <lizbethgarcialebron@gmail.com>
2023-01-09 13:58:16 +0000

    task 16 description

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,6 +2,7 @@
 /**
  * binary_tree_is_perfect - function that checks if a binary tree is perfect
  * @tree: is a pointer to the root node
+ * Return: 1 if perfect 0 is not
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
